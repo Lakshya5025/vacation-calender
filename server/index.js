da76b529814c5cce60b17451debb9b74f768d9f8
Lakshya Gupta <lakshyagupta916640@gmail.com>
2025-09-24 19:02:48 +0530

    add another path

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT;
 
 // Middleware
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:517",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 })); app.use(express.json());
