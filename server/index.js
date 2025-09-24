6dbd1577c6a7d70c9883a0cb1b125c52ae717976
Lakshya Gupta <lakshyagupta916640@gmail.com>
2025-09-24 17:20:22 +0530

    bug fix

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT;
 
 // Middleware
 app.use(cors({
-    origin: "http://localhost:517",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 })); app.use(express.json());
